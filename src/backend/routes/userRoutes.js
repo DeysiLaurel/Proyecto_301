823083e286927caeaedc8671a4661e5543ee6d18
Deysi Laurel <1624293@senati.pe>
2025-10-21 11:28:08 -0500

    feat:Creando enpoint para updateUsers

--- src/backend/routes/userRoutes.js
+++ src/backend/routes/userRoutes.js
@@ -6,9 +6,10 @@ const router = express.Router();
 //Rutas para llamar al usuario
 router.get('/',userController.getUsers);
 router.post('/',userController.createUser);
+router.put('/:id',userController.updateUser);
 //Metodo para eliminar DELETE
-//Metodo para actualizar PUT
+
 //Metodo para modificar PATCH
 
 
-export default router;
+export default router
