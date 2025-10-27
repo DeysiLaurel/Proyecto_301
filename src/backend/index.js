0a6cc39867e4968a8b1531fa262bc1db6d231746
Deysi <deysi@gmail.com>
2025-10-27 11:12:27 -0500

    feat:Enviando servicio de Email

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
-import swaggerUi from 'swagger-ui-express'; 
-import { swaggerSpec } from './swagger.js'; 
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import emailRoutes from "./routes/emailRoutes.js";
+import swaggerUi from "swagger-ui-express"; 
+import { swaggerSpec } from "./swagger.js"; 
 
 dotenv.config();
 
@@ -13,18 +14,18 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use('/api-docs', swaggerUi.serve,swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve,swaggerUi.setup(swaggerSpec));
 
 
 app.get('/',(req, res) => {
     res.json({
-        message:'Api corriendo correctamente'
+        message:"Api corriendo correctamente"
     });
 });
 
 //Rutas que deseo usar
-app.use('/api/users',userRoutes);
-
+app.use("/api/users",userRoutes);
+app.use("/api/email",emailRoutes);
 app.listen(PORT, () => {
     console.log(`Servidor corriendo y escuchando en el puerto ${PORT}`);
 });
--- src/backend/routes/emailRoutes.js
+++ src/backend/routes/emailRoutes.js
@@ -1,3 +1,35 @@
-import express from 'express';
-import {emailController } from '../controllers/emailController.js';
+import express from "express";
+import { registerUser } from "../controllers/emailController.js";
 
+const router = express.Router();
+
+router.post("/register", registerUser);
+
+/**
+ * @swagger
+ * /api/email/register:
+ *   post:
+ *     summary: Registrar usuario y enviar
+ *     tags:
+ *       - Email
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: usuario@gmail.com
+ *               name:
+ *                 type: string
+ *                 example: Juan Alimaña
+ *     responses:
+ *       200:
+ *         description: Email enviado correctamente
+ *       500:
+ *         description: Error del servicio al enviar Email
+ */
+
+export default router;
