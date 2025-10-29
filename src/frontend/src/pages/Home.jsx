8d8cdff9b0050e41c08a1160a01c86fbf6e5f995
Deysi <deysi@gmail.com>
2025-10-29 11:04:39 -0500

    feat:Creando configuracion inicial de React

---
+++ src/frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home(){
+    return(
+        <div className="min-h-screen bg-gray-100 pl-8">
+            <h1 className="text-2xl font-boald mb-4">Bienvenida a mi pagina 😁👍</h1>
+            <p1 className="text-orange-600 text-xl">Esta es nuestra aplicacion con React 😊</p1>
+            
+        </div>
+
+    );
+}
+export default Home;
